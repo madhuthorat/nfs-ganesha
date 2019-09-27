583af0d3a1dfbfe7a1c03a082122436f77d2dd33
Madhu Thorat <madhu.punjabi@in.ibm.com>
2019-09-27 05:07:40 -0400

    MDCACHE - Additional debug code for Entry LRU Refcounts
    This is debug code that helps with additional tracking of
    mdcache_lru_get() and mdcache_alloc_and_check_handle() functions to
    track down refcount errors for MDCACHE entries.
    As mdcache_lru_get() and mdcache_alloc_and_check_handle()
    function/function callers get called from multiple places in code, this
    additional tracking would help to know their callers.
    
    Change-Id: I032ee44bb0ea5ccf3b1d7470475ded0e71b9b668
    Signed-off-by: Madhu Thorat <madhu.punjabi@in.ibm.com>
    (cherry picked from commit 75f9a9075f001f3160ca0af98d850e9bcc5a8b6a)
    
    Conflicts:
    	src/FSAL/Stackable_FSALs/FSAL_MDCACHE/mdcache_handle.c
    	src/FSAL/Stackable_FSALs/FSAL_MDCACHE/mdcache_helpers.c
    	src/FSAL/Stackable_FSALs/FSAL_MDCACHE/mdcache_lru.h

--- src/FSAL/Stackable_FSALs/FSAL_MDCACHE/mdcache_helpers.c
+++ src/FSAL/Stackable_FSALs/FSAL_MDCACHE/mdcache_helpers.c
@@ -171,7 +171,7 @@ static mdcache_entry_t *_mdcache_alloc_handle(
 	mdcache_entry_t *result;
 	fsal_status_t status;
 
-	result = mdcache_lru_get(sub_handle);
+	result = mdcache_lru_get(sub_handle, func, line);
 
 	if (result == NULL) {
 		/* Should never happen, but our caller will handle... */
@@ -706,8 +706,8 @@ _mdcache_new_entry(struct mdcache_fsal_export *export,
 	/* We did not find the object.  Pull an entry off the LRU. The entry
 	 * will already be mapped.
 	 */
-	nentry = mdcache_alloc_handle(export, sub_handle, sub_handle->fs,
-				      reason);
+	nentry = _mdcache_alloc_handle(export, sub_handle, sub_handle->fs,
+				       reason, func, line);
 
 	if (nentry == NULL) {
 		/* We didn't get an entry because of unexport in progress,
@@ -1411,7 +1411,8 @@ fsal_status_t mdc_lookup_uncached(mdcache_entry_t *mdc_parent,
 	status = mdcache_alloc_and_check_handle(export, sub_handle, &new_obj,
 						false, &attrs, attrs_out,
 						"lookup ", mdc_parent, name,
-						&invalidate, NULL);
+						&invalidate, NULL,
+						__func__, __LINE__);
 
 	fsal_release_attrs(&attrs);
 
